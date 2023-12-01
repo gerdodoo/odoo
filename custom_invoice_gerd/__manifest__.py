5c613723dc84af69da59ac7b3b5214ea47563a9f
gerdodoo <152659336+gerdodoo@users.noreply.github.com>
2023-12-01 16:34:45 -0600

    Add files via upload

---
+++ custom_invoice_gerd/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Custom Invoice Report GERV',
+    'version': '1.1',
+    'description': ''' Formatos personalizado para totales en reporte de factura
+    ''',
+    'category': 'Contabilidad',
+    'author': 'IDCA',
+    'website': '',
+    'depends': [
+        'account'
+    ],
+    'data': [
+        'report/invoice_document_custom.xml',
+        
+    ],
+    'images': [],
+    'application': False,
+    'installable': True,
+    'price': 0.00,
+    'currency': 'USD',
+    'license': 'OPL-1',
+}
