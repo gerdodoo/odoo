030fb09680d8d8778847d6a967c66b149f42422c
gerdodoo <152659336+gerdodoo@users.noreply.github.com>
2025-02-19 12:25:18 -0600

    Update manseq.py

--- maintenance_seq_brame/models/manseq.py
+++ maintenance_seq_brame/models/manseq.py
@@ -3,7 +3,7 @@ from odoo import models, fields, api
 class MaintenanceEquipment(models.Model):
     _inherit = 'maintenance.equipment'
 
-    sequence = fields.Char(string='Referencia', required=True, copy=False, readonly=True)
+    sequence = fields.Char(string='Referencia', required=True, copy=False, readonly=True, default='/')
 
     @api.model
     def _get_next_sequence(self):
