379d36c891035bdc440572cdc9c87c52fc7335f0
gerdodoo <152659336+gerdodoo@users.noreply.github.com>
2025-02-21 17:34:18 -0600

    Update cratinli.py

--- account_invoice_line_report/models/cratinli.py
+++ account_invoice_line_report/models/cratinli.py
@@ -4,12 +4,12 @@ class AccountInvoiceReport(models.Model):
     _inherit = "account.invoice.report"
 
     analytic_account_id = fields.Many2one(
-        comodel_name="account.analytic.account",
+        comodel_name="analytic.distribution",
         string="Cuenta Analítica",
     )
 
     def _select(self):
-        return super()._select() + ", line.analytic_account_id as analytic_account_id"
+        return super()._select() + ", line.analytic_distribution as analytic_distribution"
 
     def _group_by(self):
-        return super()._group_by() + ", line.analytic_account_id"
+        return super()._group_by() + ", line.analytic_distribution"
