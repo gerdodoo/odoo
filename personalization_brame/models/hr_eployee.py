f1178ddaf74e4074bc0051a719074521920736f2
gerdodoo <152659336+gerdodoo@users.noreply.github.com>
2025-02-27 15:14:51 -0600

    Update hr_eployee.py

--- personalization_brame/models/hr_eployee.py
+++ personalization_brame/models/hr_eployee.py
@@ -2,6 +2,8 @@ import re
 from odoo import models, api
 from odoo.exceptions import ValidationError
 
+# /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/
+
 class HrEmployee(models.Model):
     _inherit = "hr.employee"  # 🔹 Heredamos el modelo de empleados
 
@@ -11,3 +13,4 @@ class HrEmployee(models.Model):
         for record in self:
             if record.curp and not re.match(curp_regex, record.curp):
                 raise ValidationError("La CURP ingresada no es válida. Verifica su formato.")
+
