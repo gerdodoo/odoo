2d2502256fbf5ef66debd4e9982a5aa25d4d64e5
gerdodoo <152659336+gerdodoo@users.noreply.github.com>
2025-02-26 18:21:38 -0600

    Update account_move_line.py

--- account_line_view/models/account_move_line.py
+++ account_line_view/models/account_move_line.py
@@ -19,13 +19,33 @@
 #    If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from odoo import models, fields
-
+from odoo import models, fields, api
 
 class AccountMoveLine(models.Model):
-    """Inherit model account move line for adding new field"""
-    _inherit = 'account.move.line'
-    _description = "Inherit model account move line"
+    """Inherit model account move line for adding new fields"""
+    _inherit = "account.move.line"
 
-    product_image = fields.Binary(related='product_id.image_1920', string="Product Image",
-                                  help="Product image")
+    product_image = fields.Binary(
+        related="product_id.image_1920",
+        string="Product Image",
+        help="Product image"
+    )
+
+    analytic_account_code = fields.Char(
+        string="Código Cuenta Analítica",
+        compute="_compute_analytic_account_code",
+        store=True
+    )
+
+    @api.depends('analytic_distribution')
+    def _compute_analytic_account_code(self):
+        for line in self:
+            if line.analytic_distribution:
+                analytic_ids = list(line.analytic_distribution.keys())  # Extrae los IDs de cuentas analíticas
+                if analytic_ids:
+                    analytic_account = self.env['account.analytic.account'].browse(int(analytic_ids[0]))  # Obtiene la cuenta analítica
+                    line.analytic_account_code = analytic_account.code if analytic_account else ''
+                else:
+                    line.analytic_account_code = ''
+            else:
+                line.analytic_account_code = ''
